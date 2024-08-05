51458d6d7bc23580e18f2854895b05de380ed70d
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-08-05 14:04:07 +0530

    fix: extra CR

--- app/main.js
+++ app/main.js
@@ -4,14 +4,14 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     let [requestLine, rawHeaders, body] = data.toString().split("\r\n\r\n");
     let [method, path, version] = requestLine.split(" ");
-    
+
     let splitted = path.split("/");
     if (splitted[1] == "echo") {
       let responeHeaders = new Headers({
         "Content-Type": "text/plain",
         "Content-Length": splitted[2].length,
       });
-      socket.write("HTTP/1.1 200 OK\r\n\r\n" + responeHeaders.toString() + "\r\n\r\n" + splitted[2]);
+      socket.write("HTTP/1.1 200 OK\r\n" + responeHeaders.toString() + "\r\n\r\n" + splitted[2]);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
