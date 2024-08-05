41f2edac29e24cfddd86987732ca71ed68e934ec
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-08-05 13:45:03 +0530

    feat: handle url paths

--- app/main.js
+++ app/main.js
@@ -1,6 +1,15 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    let [requestLine, headers, body] = data.toString().split("\r\n");
+    let [method, path, version] = requestLine.split(" ");
+    if(path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  });
   socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
