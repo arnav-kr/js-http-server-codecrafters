3484bd870ce3c7bf72d32fb7cdf9dd790e9372e0
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-08-05 13:46:25 +0530

    fix: previous header still present

--- app/main.js
+++ app/main.js
@@ -10,7 +10,6 @@ const server = net.createServer((socket) => {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
   });
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
   });
