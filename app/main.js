db1adeac95e6b1d4d1f6aa6e7ee58a111573c7da
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-08-05 14:02:31 +0530

    feat: sending responses

--- app/main.js
+++ app/main.js
@@ -2,10 +2,16 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    let [requestLine, headers, body] = data.toString().split("\r\n");
+    let [requestLine, rawHeaders, body] = data.toString().split("\r\n\r\n");
     let [method, path, version] = requestLine.split(" ");
-    if(path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    
+    let splitted = path.split("/");
+    if (splitted[1] == "echo") {
+      let responeHeaders = new Headers({
+        "Content-Type": "text/plain",
+        "Content-Length": splitted[2].length,
+      });
+      socket.write("HTTP/1.1 200 OK\r\n\r\n" + responeHeaders.toString() + "\r\n\r\n" + splitted[2]);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
@@ -16,3 +22,26 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, "localhost");
+
+class Headers {
+  constructor(headers = {}) {
+    this.headers = (typeof headers) == "string" ? this._parseRawHeaders(headers) : headers;
+  }
+  get(name) {
+    return this.headers[name];
+  }
+  set(name, value) {
+    this.headers[name] = value;
+  }
+  toString() {
+    return Object.keys(this.headers).map((name) => `${name}: ${this.headers[name]}`).join("\r\n");
+  }
+  _parseRawHeaders(rawHeaders) {
+    let headers = {};
+    rawHeaders.split("\r\n").forEach((header) => {
+      let [name, value] = header.split(": ");
+      headers[name] = value;
+    });
+    return headers;
+  }
+}
